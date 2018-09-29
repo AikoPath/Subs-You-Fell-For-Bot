693c69f6a2694e95f5a5d7e855c24bb0b5e2b493
Aiko Klostermann <AikoK@thoughtworks.com>
2018-09-29 23:09:13 +0100

    Use CONFIG to pass credentials to PRAW; read comments

--- subs_u_fell_4.py
+++ subs_u_fell_4.py
@@ -1,7 +1,17 @@
 import praw
-import pandas as pd
-import datetime as dt
+import json
+
+with open('CONFIG', 'r') as f:
+    config = json.load(f)
+
+reddit = praw.Reddit(client_id=config['REDDIT']['PERSONAL_USE_SCRIPT'],
+                     client_secret=config['REDDIT']['SECRET_KEY'],
+                     user_agent=config['REDDIT']['APP_NAME'],
+                     username=config['REDDIT']['USER_NAME'],
+                     password=config['REDDIT']['PASSWORD'])
 
 
-
-
+if __name__ == '__main__':
+    for comment in reddit.subreddit('all').stream.comments():
+        if comment.body.encode("utf-8").lower() == '/r/subsyoufellfor':
+            print(comment)
