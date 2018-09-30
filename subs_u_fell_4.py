3a4235f588ca2756e28e81fff473b367bf3a0bd1
Aiko Klostermann <AikoK@thoughtworks.com>
2018-09-30 21:07:10 +0100

    Introduce screenshot capability

--- subs_u_fell_4.py
+++ subs_u_fell_4.py
@@ -1,5 +1,6 @@
 import praw
 import json
+from selenium import webdriver
 
 with open('CONFIG', 'r') as f:
     config = json.load(f)
@@ -11,7 +12,16 @@ reddit = praw.Reddit(client_id=config['REDDIT']['PERSONAL_USE_SCRIPT'],
                      password=config['REDDIT']['PASSWORD'])
 
 
+def takeScreenshot(comment_permalink):
+    driver = webdriver.Chrome('webdriver/chromedriver')
+    driver.get('https://reddit.com{}?context=2&depth=3'.format(comment_permalink))
+    driver.save_screenshot('my_screenshot.png')
+#   TODO driver.get_screenshot_as_png()
+    driver.quit()
+
+
 if __name__ == '__main__':
     for comment in reddit.subreddit('all').stream.comments():
         if comment.body.encode("utf-8").lower() == '/r/subsyoufellfor':
-            print(comment)
+            print('Found one at {}'.format(comment.permalink))
+            takeScreenshot(comment.permalink)
